96398ff7fc8671c4f80f11e080b1c0daf31b48d7
yeji <oyatpeach@naver.com>
2023-07-08 15:58:10 +0900

    Feat : 구독 종류에 따른 페이지 생성

--- src/app/mypage/subscribe/request2/page.tsx
+++ src/app/mypage/subscribe/request2/page.tsx
@@ -1,7 +1,176 @@
+'use client';
 import React from 'react';
+import styled from 'styled-components';
+import Button from '@/components/common/Button';
+import check_off from '@/assets/images/check/off.svg';
+import check_on from '@/assets/images/check/on.svg';
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 function page() {
-  return <div>2</div>;
+  const router = useRouter();
+
+  const [select, setSelect] = useState(false);
+
+  const selectBasic = (e: any) => {
+    setSelect(!select);
+  };
+
+  return (
+    <Container>
+      <Title>{'리픽 멤버십 구독 결제'}</Title>
+      <SemiTitle>
+        {'리픽 멤버십 구독을 통해 온라인 제품을 홈피팅 후 옷을 구매해보세요!'}
+      </SemiTitle>
+      <Wrapper>
+        <Box>
+          <Content>
+            <Plan>{'Pro Plan'}</Plan>
+
+            <DiscountWrapper>
+              <Price>{'25,900원'}</Price>
+              <Line />
+
+              <PriceWrapper>
+                <Discount>{'월 15,540원'}</Discount>
+                <Tax>{'(부가세 포함)'}</Tax>
+              </PriceWrapper>
+            </DiscountWrapper>
+
+            <Info>
+              {'3벌의 의류를 선택가능합니다.*'}
+              <br />
+              {
+                ' 모든 가격은 부가가치세가 포함된 가격이며, 추가로 결제되는 금액은 없습니다.*'
+              }
+            </Info>
+          </Content>
+        </Box>
+      </Wrapper>
+
+      <Notice>
+        <li>{'맴버십 구매에 따른 공지문입니다.'}</li>
+        <li>{'따로 기획팀과 의논해 추가할 예정입니다.'}</li>
+        <li>{'한 세 줄 정도로 가정하고 만들었습니다.'}</li>
+      </Notice>
+
+      <Agree>
+        <Check onClick={(e: any) => selectBasic(e)}>
+          {select ? <On src={check_on.src} /> : <Off src={check_off.src} />}
+        </Check>
+        <div className="agree">{'매월 정기 결제에 동의합니다.'}</div>
+      </Agree>
+      <div className="button">
+        <div onClick={() => router.push('/mypage/subscribe')}>
+          <Button content="뒤로 가기" />
+        </div>
+        <Button content="결제 하기" />
+      </div>
+    </Container>
+  );
 }
 
 export default page;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .button {
+    display: flex;
+    width: 744px;
+    justify-content: space-between;
+    margin-top: 70px;
+    margin-bottom: 148px;
+  }
+`;
+const Title = styled.div`
+  font-size: 36px;
+  font-weight: 600;
+  margin-top: 120px;
+  text-align: center;
+`;
+const SemiTitle = styled.div`
+  font-size: 20px;
+  font-weight: 400;
+  text-align: center;
+`;
+const Wrapper = styled.div`
+  display: flex;
+  margin-top: 60px;
+`;
+const Box = styled.div`
+  border-radius: 15px;
+  background: var(--5, #fff);
+  box-shadow: 0px 4px 24px 0px rgba(0, 0, 0, 0.16);
+  width: 1216px;
+  height: 260px;
+`;
+const Content = styled.div`
+  margin-left: 101px;
+  margin-top: 42px;
+`;
+const Plan = styled.div`
+  font-weight: 600;
+  font-size: 24px;
+`;
+const Price = styled.div`
+  font-weight: 600;
+  font-size: 48px;
+`;
+const Discount = styled.div`
+  font-weight: 600;
+  font-size: 48px;
+`;
+const Info = styled.div`
+  font-weight: 400;
+  font-size: 16px;
+  margin-top: 30px;
+`;
+
+const DiscountWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin-top: 12px;
+  position: relative;
+`;
+const PriceWrapper = styled.div`
+  display: flex;
+  margin-left: 24px;
+`;
+const Tax = styled.div`
+  font-weight: 600;
+  font-size: 20px;
+  margin-top: 30px;
+  margin-left: 12px;
+`;
+const Line = styled.div`
+  width: 204px;
+  height: 5px;
+  background: var(--unnamed, #ff3d00);
+  position: absolute;
+`;
+const Notice = styled.div`
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 400;
+  text-align: center;
+  margin-top: 60px;
+`;
+const Check = styled.div``;
+const Off = styled.img`
+  margin-bottom: 24px;
+`;
+const On = styled.img`
+  margin-bottom: 24px;
+`;
+const Agree = styled.div`
+  display: flex;
+
+  margin-top: 102px;
+  .agree {
+    font-size: 24px;
+    font-weight: 600;
+    margin-left: 24px;
+  }
+`;
