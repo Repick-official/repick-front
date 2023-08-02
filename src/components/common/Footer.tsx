6314a74cfb4f3d6a0acd35ef58b3658be628077e
yeji <oyatpeach@naver.com>
2023-08-02 18:44:35 +0900

    Style : 옷장 정리, 푸터 코드 및 스타일 정리

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,27 +1,26 @@
-import React from 'react';
 import logo from '@/assets/images/navigation/logo.svg';
 import styled from 'styled-components';
 
 function Footer() {
   return (
     <Container>
-      <Q>
-        <Wrapper>
-          <Content>
-            <P>
-              <Bold>{'헬프 센터'}</Bold>
-              <Bold> {'운영 정책'}</Bold>
-            </P>
-            <N>
-              <I>
-                <Info>{'자주 하는 질문'}</Info>
-                <Info>{'1:1 문의'}</Info>
-              </I>
-              <I>
-                <Info>{'개인정보 처리 방침'}</Info>
-              </I>
-            </N>
-          </Content>
+      <Wrapper.Wrap>
+        <Wrapper.Content>
+          <Content.Wrapper>
+            <Content.Help>
+              <Content.Bold>{'헬프 센터'}</Content.Bold>
+              <Content.Bold> {'운영 정책'}</Content.Bold>
+            </Content.Help>
+            <Content.InfoWrapper>
+              <Content.InfoWrap>
+                <Content.Info>{'자주 하는 질문'}</Content.Info>
+                <Content.Info>{'1:1 문의'}</Content.Info>
+              </Content.InfoWrap>
+              <Content.InfoWrap>
+                <Content.Info>{'개인정보 처리 방침'}</Content.Info>
+              </Content.InfoWrap>
+            </Content.InfoWrapper>
+          </Content.Wrapper>
           <Logo src={logo.src} />
           <Do>
             {'Repick | 대표 : 이도현 | 개인정보담당자 : 서찬혁'}
@@ -33,57 +32,60 @@ function Footer() {
           <CopyRight>
             {'Copyright (c) 2023 Repick 리픽 All rights Reserved.'}
           </CopyRight>
-        </Wrapper>
-      </Q>
+        </Wrapper.Content>
+      </Wrapper.Wrap>
     </Container>
   );
 }
 
 export default Footer;
 
-const Q = styled.div`
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  background-color: rgba(246, 246, 246, 1);
-  height: 364px;
-  margin-top: auto;
-`;
 const Container = styled.div``;
-const Wrapper = styled.div`
-  width: 1216px;
-  margin-top: 53px;
-`;
-const Content = styled.div`
-  display: flex;
-`;
-const App = styled.div`
-  margin-left: 262px;
-`;
-const Bold = styled.div`
-  font-size: 16px;
-  font-weight: 600;
-  color: var(--1, #111);
-  margin-bottom: 8px;
-  line-height: 140%;
-`;
-const Info = styled.div`
-  margin-right: 24px;
-  font-size: 16px;
-  color: rgba(95, 95, 95, 1);
-  line-height: 140%;
-`;
 
-const I = styled.div`
-  display: flex;
-  margin-bottom: 8px;
-  margin-left: 24px;
-`;
-const P = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-const N = styled.div``;
+const Wrapper = {
+  Wrap: styled.div`
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    background-color: rgba(246, 246, 246, 1);
+    height: 364px;
+    margin-top: auto;
+  `,
+  Content: styled.div`
+    width: 1216px;
+    margin-top: 53px;
+  `,
+};
+
+const Content = {
+  Wrapper: styled.div`
+    display: flex;
+  `,
+  Help: styled.div`
+    display: flex;
+    flex-direction: column;
+  `,
+  Bold: styled.div`
+    font-size: 16px;
+    font-weight: 600;
+    color: var(--1, #111);
+    margin-bottom: 8px;
+    line-height: 140%;
+  `,
+  InfoWrapper: styled.div``,
+  InfoWrap: styled.div`
+    display: flex;
+    margin-bottom: 8px;
+    margin-left: 24px;
+  `,
+  Info: styled.div`
+    margin-right: 24px;
+    font-size: 16px;
+    color: rgba(95, 95, 95, 1);
+    line-height: 140%;
+  `,
+};
+
 const Logo = styled.img`
   width: 106px;
   height: 33px;
@@ -100,30 +102,4 @@ const CopyRight = styled.div`
   margin-top: 19px;
   color: rgba(95, 95, 95, 1);
   line-height: 140%;
-`;
-const Down = styled.div`
-  font-size: 19px;
-  font-weight: 700;
-  line-height: 36px;
-  letter-spacing: -0.5px;
-  margin-top: 53px;
-  margin-bottom: 12px;
-`;
-const A = styled.div`
-  border-radius: 15px;
-  background: var(--3, #b4b4b4);
-  display: flex;
-  width: 140px;
-  height: 48px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  flex-shrink: 0;
-  color: rgba(232, 232, 232, 1);
-  font-weight: 600;
-  font-size: 16px;
-  margin-right: 18px;
-`;
-const W = styled.div`
-  display: flex;
 `;
